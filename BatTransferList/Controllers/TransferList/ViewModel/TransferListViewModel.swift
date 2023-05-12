e86b3bba1889a3441d204bd3560427542bce2231
Siamak Rostami <srostami2005@gmail.com>
2023-05-12 12:42:31 +0330

    feat: added appIcon and some assets

--- BatTransferList/Controllers/TransferList/ViewModel/TransferListViewModel.swift
+++ BatTransferList/Controllers/TransferList/ViewModel/TransferListViewModel.swift
@@ -5,14 +5,15 @@
 //  Created by Siamak Rostami on 5/12/23.
 //
 
-import Foundation
 import Combine
+import Foundation
+
+// MARK: - TransferListViewModel
 
 class TransferListViewModel: BaseViewModel<DependencyContainer> {
-    
-    var container: DependencyContainer
-    
-    init(dependency: DependencyContainer){
+    // MARK: Lifecycle
+
+    init(dependency: DependencyContainer) {
         self.container = dependency
         super.init(dependency: self.container)
         getTransferList()
@@ -21,31 +22,47 @@ class TransferListViewModel: BaseViewModel<DependencyContainer> {
     required init(dependency: Dependency) {
         fatalError("init(dependency:) has not been implemented")
     }
+
+    // MARK: Internal
+
+    var container: DependencyContainer
     
     var currentPage: Int = 1
-    var transferList = CurrentValueSubject<[TransferListModel]?,Never>(nil)
-
+    var transferList = CurrentValueSubject<[TransferListModel]?, Never>(nil)
 }
 
 extension TransferListViewModel {
-    
-    func getTransferList(){
+    func getTransferList() {
+        if self.currentPage == 1 {
+            self.transferList.send(nil)
+        }
         self.container.networkServices.transferListServices?
             .getTransferList(page: self.currentPage)
             .subscribe(on: DispatchQueue.main)
             .sink(receiveCompletion: { [weak self] completion in
-                switch completion{
+                switch completion {
                 case .failure(let error):
                     self?.error.send(error)
                 default:
                     break
                 }
             }, receiveValue: { [weak self] model in
-                self?.transferList.send(model)
+                self?.createTransferArrayFromModel(model: model)
+
             })
             .store(in: &self.disboseBag)
-  
     }
-  
+    
+    private func createTransferArrayFromModel(model: [TransferListModel]?) {
+        guard let model = model else {
+            self.transferList.send(nil)
+            return
+        }
+        if self.transferList.value == nil {
+            self.transferList.send(.init())
+        }
+        model.forEach {
+            self.transferList.value?.append($0)
+        }
+    }
 }
-
